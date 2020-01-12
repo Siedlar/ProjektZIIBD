13f1441f1e747df1849dd0e7a69cad8cb8829c9c
Siedlar <ksiedlarski@gmail.com>
2020-01-12 21:25:01 +0100

    Dodanie Toggle Button i zaimplementowanie onClick dla Buttona

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Controller {
+
+    @FXML
+   private Button Przycisk;
+    @FXML
+    private TextArea textArea1;
+
+    @FXML
+    public void onClick(){
+        System.out.println("on ekszyn");
+        Przycisk.setText("ESZKERE");
+        ResultSet resultSet=QueryExec.executeSelect("Select * from employees");
+        try {
+            while(resultSet.next()){
+                String x=resultSet.getString(2);
+                textArea1.setText("imie to:"+x);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
